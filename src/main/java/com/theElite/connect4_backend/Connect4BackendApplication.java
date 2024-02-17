cb5441e88d7ff6ed7440e9cf3b4abcb19bb1a0e3
Suraj Gupta <surajrocks1997@gmail.com>
2024-02-17 19:14:43 +0530

    initial commit

---
+++ src/main/java/com/theElite/connect4_backend/Connect4BackendApplication.java
@@ -0,0 +1,13 @@
+package com.theElite.connect4_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Connect4BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Connect4BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theElite/connect4_backend/Connect4BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theElite.connect4_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Connect4BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
